422c5fe9fc1641413e77e2c0db5954ca30029f0a
premrakh-vision <premrakh.vision@gmail.com>
2024-02-09 11:51:46 +0530

    change db to postgres

--- ad_project/settings.py
+++ ad_project/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'ad_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adsense',
+        'USER': 'adsense',
+        'PASSWORD': '73PQJoF[1=@0',
+        'HOST': '23.21.55.122'
     }
 }
+
+CONN_MAX_AGE = 300 
 
 
 # Password validation
